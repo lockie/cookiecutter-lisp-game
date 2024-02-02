8c5500c52778ef9bf596eb4867e99de8be4e8724
Andrew Kravchuk <awkravchuk@gmail.com>
2024-02-02 21:43:01 +0100

    Added build system MacOS support 🎉 and other minor improvements

--- {{cookiecutter.project_slug}}/package.sh
+++ {{cookiecutter.project_slug}}/package.sh
@@ -10,7 +10,7 @@ fi
 export VERSION=${GITHUB_REF_NAME:-$(git describe --always --tags --dirty=+ --abbrev=6)}
 
 function do_build () {
-    sbcl --dynamic-space-size 2048 --disable-debugger --quit --load package/build.lisp
+    CL_SOURCE_REGISTRY=$(pwd) sbcl --dynamic-space-size 2048 --disable-debugger --quit --load package/build.lisp
 }
 
 case $1 in
@@ -27,19 +27,34 @@ case $1 in
         ;;
 
     windows)
-        if ! command -v mingw-ldd > /dev/null 2>&1
-        then
-            echo "Missing mingw-ldd helper binary"
-            exit 1
-        fi
         do_build
-        for binary in bin/*; do
-            echo -n "${PATH}" | tr ';' '\0' | \
-                xargs -t0 mingw-ldd "$binary" --disable-multiprocessing --dll-lookup-dirs | \
-                { grep -v -e 'not found' -e 'system32' || test $? = 1; } | \
-                awk -F '=> ' '{ print $2 }' | xargs -I deps cp deps bin/
-        done
+        ntldd -R bin/* | grep ucrt64 | awk -F '=> ' '{ print $2 }' | awk '{ print $1 }' | xargs -I deps cp deps bin
         makensis package/installer.nsi
+        ;;
+
+    macos)
+        do_build
+        bundle="{{cookiecutter.project_name}}.app"
+        contents=$bundle/Contents
+        mkdir -p "$contents/MacOS"
+        cp -r Resources "$contents"
+        cp package/Info.plist "$contents"
+        cp package/icon.png "$contents/Resources"
+        for library in bin/*.dylib; do
+            dylibbundler -of -cd -b -p '@loader_path' -x "$library" -d "$contents/MacOS"
+            cp "$library" "$contents/MacOS"
+        done
+
+        # https://bugs.launchpad.net/sbcl/+bug/1869401
+        replace_fr=$(echo -n  "/opt/local/lib/libzstd.1.dylib" | xxd -ps -c1 | tr -d '\n')
+        replace_to=$(echo -en "@loader_path/libzstd.1.dylib\x00\x00" | xxd -ps -c1 | tr -d '\n')
+        xxd -ps -c1 bin/{{cookiecutter.project_slug}} | tr -d '\n' | sed "s/$replace_fr/$replace_to/" | fold -w 2 | xxd -r -p > "$contents/MacOS/{{cookiecutter.project_slug}}"
+        chmod +x "$contents/MacOS/{{cookiecutter.project_slug}}"
+
+        hdiutil create -quiet -srcfolder "$bundle" out.dmg
+        # NOTE: ULMO = lzma compression = Catalina+ only
+        hdiutil convert -quiet out.dmg -format ULMO -o "{{cookiecutter.project_slug}}-${VERSION}.dmg"
+        rm out.dmg
         ;;
 
     *)
--- {{cookiecutter.project_slug}}/package/build.lisp
+++ {{cookiecutter.project_slug}}/package/build.lisp
@@ -1,4 +1,9 @@
 (proclaim '(optimize (speed 3) (safety 0) (debug 0) (compilation-speed 0)))
-(pushnew (uiop:getcwd) asdf:*central-registry*)
+(ql-util:without-prompting (ql:update-all-dists))
 (ql:quickload '(#:{{cookiecutter.project_slug}} #:deploy))
+{% if cookiecutter.backend == "SDL2" %}
+#+darwin (progn
+           (deploy:define-library cl-glut::glut :dont-deploy t)
+           (deploy:define-library cl-opengl-bindings::opengl :dont-deploy t))
+{% endif %}
 (asdf:make :{{cookiecutter.project_slug}})
