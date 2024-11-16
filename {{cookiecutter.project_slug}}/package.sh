98d92eaa434b190b19feacc3a5cb1cccded3504c
Andrew Kravchuk <awkravchuk@gmail.com>
2024-11-16 15:54:01 +0100

    A few more tweaks for Windows build

--- {{cookiecutter.project_slug}}/package.sh
+++ {{cookiecutter.project_slug}}/package.sh
@@ -32,8 +32,8 @@ case $1 in
         {% if cookiecutter.backend != "raylib" %}
         ntldd -R bin/* | grep ucrt64 | awk -F '=> ' '{ print $2 }' | awk '{ print $1 }' | sed 's/\\/\\\\/g' | xargs -I deps cp deps bin
         {% endif %}
-        (cd "$TEMP"; convert "$OLDPWD/package/icon.png" -define icon:auto-resize=16,32,48,64,256 icon.ico)
-        (cd "$TEMP"; convert -resize 150x57 -extent 150x57 -gravity center -background white -alpha remove -alpha off "$OLDPWD/package/icon.png" BMP2:icon.bmp)
+        magick package/icon.png -define icon:auto-resize=16,32,48,64,256 ICO:- > "$TEMP/icon.ico"
+        magick package/icon.png -resize 150x57 -extent 150x57 -gravity center -background white -alpha remove -alpha off BMP2:- > "$TEMP/icon.bmp"
         makensis package/installer.nsi
         ;;
 
